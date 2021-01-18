5a8c6179a91bbbc837a10e87fa425aff6ef4952b
Machetta Gregg R <machettagreggr@johndeere.com>
2021-01-18 10:57:16 -0600

    Item 1 sucessful

--- script.js
+++ script.js
@@ -7,16 +7,46 @@ function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
 
-//pull API - use CORS link if needed
+// Get item 1 -  Burgers 
+function getItem() {
+
+//select API - use CORS link if needed
 
 let url = 'https://foodish-api.herokuapp.com/api/images/burger'
-let response;
-// Use Fetch command 
+let response; 
+
+// Use Fetch command to Pull API & console Success
 
 fetch(url) // Fetch URL 
-  .then(response => response.json()) // return a JSON response
+  .then(response => response.json()) // return data a JSON response in a string
+  .then (json=> {document.querySelector(".foodPicture").src =json.image;})
 
   // console.log successful reponse data or specific response.name
-  .then(response => console.log("success!", response))
+  //.then(response => console.log("success!", response))
   // catch error if not successful response
   .catch(err => console.log('Something Wrong..', err))
+}
+ 
+
+getItem();
+
+//get Item 2 - Sandwiches
+ function getItem2() {
+
+  //select API - use CORS link if needed
+  
+  let url = 'https://foodish-api.herokuapp.com/api/images/burger'
+  let response2; 
+  
+  // Use Fetch command to Pull API & console Success
+  
+  fetch(url) // Fetch URL 
+    .then(response => response2.json()) // return data a JSON response in a string
+    .then (json=> {document.querySelector(".foodPicture").src =json.image;})
+  
+    // console.log successful reponse data or specific response.name
+    //.then(response => console.log("success!", response))
+    // catch error if not successful response
+    .catch(err => console.log('Something Wrong..', err))
+  }
+   getItem2();
