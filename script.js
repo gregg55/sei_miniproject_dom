023fa85dc54299209467418b68534c6122d161d5
Machetta Gregg R <machettagreggr@johndeere.com>
2021-01-18 09:33:37 -0600

    successful fetch command & pulled image

--- script.js
+++ script.js
@@ -10,15 +10,13 @@ function closeNav() {
 //pull API - use CORS link if needed
 
 let url = 'https://foodish-api.herokuapp.com/api/images/burger'
-
+let response;
 // Use Fetch command 
-function getFood() {
+
 fetch(url) // Fetch URL 
   .then(response => response.json()) // return a JSON response
-
 
   // console.log successful reponse data or specific response.name
   .then(response => console.log("success!", response))
   // catch error if not successful response
   .catch(err => console.log('Something Wrong..', err))
-}
