0894f9d3919d471ee7ee9fe6de67beb9036a5f2e
Machetta Gregg R <machettagreggr@johndeere.com>
2021-01-18 11:07:04 -0600

    Items 2 -4 successful

--- script.js
+++ script.js
@@ -35,13 +35,13 @@ getItem();
 
   //select API - use CORS link if needed
   
-  let url = 'https://foodish-api.herokuapp.com/api/images/burger'
+  let url2 = 'https://foodish-api.herokuapp.com/api/images/burger'
   let response2; 
   
   // Use Fetch command to Pull API & console Success
   
-  fetch(url) // Fetch URL 
-    .then(response => response2.json()) // return data a JSON response in a string
+  fetch(url2) // Fetch URL 
+    .then(response2 => response2.json()) // return data a JSON response in a string
     .then (json=> {document.querySelector(".foodPicture").src =json.image;})
   
     // console.log successful reponse data or specific response.name
@@ -50,3 +50,46 @@ getItem();
     .catch(err => console.log('Something Wrong..', err))
   }
    getItem2();
+
+
+   //get Item 3 - Special
+ function getItem3() {
+
+  //select API - use CORS link if needed
+  
+  let url3 = 'https://foodish-api.herokuapp.com/api/images/burger'
+  let response3; 
+  
+  // Use Fetch command to Pull API & console Success
+  
+  fetch(url3) // Fetch URL 
+    .then(response3 => response3.json()) // return data a JSON response in a string
+    .then (json=> {document.querySelector(".foodPicture").src =json.image;})
+  
+    // console.log successful reponse data or specific response.name
+    //.then(response => console.log("success!", response))
+    // catch error if not successful response
+    .catch(err => console.log('Something Wrong..', err))
+  }
+   getItem3();
+
+   //get Item 4 - Gourmet
+ function getItem4() {
+
+  //select API - use CORS link if needed
+  
+  let url4 = 'https://foodish-api.herokuapp.com/api/images/burger'
+  let response4; 
+  
+  // Use Fetch command to Pull API & console Success
+  
+  fetch(url4) // Fetch URL 
+    .then(response4 => response4.json()) // return data a JSON response in a string
+    .then (json=> {document.querySelector(".foodPicture").src =json.image;})
+  
+    // console.log successful reponse data or specific response.name
+    //.then(response => console.log("success!", response))
+    // catch error if not successful response
+    .catch(err => console.log('Something Wrong..', err))
+  }
+   getItem4();
