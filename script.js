5ed5d8ea6f5ff365a009cb09e9e52a8b766e6bc6
Machetta Gregg R <machettagreggr@johndeere.com>
2021-01-18 12:39:25 -0600

    semi-complete

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ fetch(url) // Fetch URL
   .then(response => response.json()) // return data a JSON response in a string
   .then (json=> {document.querySelector(".foodPicture").src =json.image;})
 
-  // console.log successful reponse data or specific response.name
+  //console.log successful reponse data or specific response.name
   //.then(response => console.log("success!", response))
   // catch error if not successful response
   .catch(err => console.log('Something Wrong..', err))
